6aa15fabdab3afcfe76e0a92a3864bbcacf0bc3c
darenky <kuyesdaren@gmail.com>
2024-11-01 13:57:50 +0200

    small change

--- lab5/lup.py
+++ lab5/lup.py
@@ -3,6 +3,7 @@ from vector import Vector
 from copy import deepcopy
 
 class LUPDecomposition:
+    
     def __init__(self, matrix):
         self.matrix = matrix
         self.n = matrix.rows
@@ -22,17 +23,20 @@ class LUPDecomposition:
                 if abs(A[i][k]) > abs(A[k_max][k]):
                     k_max = i
             
-            if A[k_max][k] == 0:
+            if A[k_max][k] == 0: 
                 raise ValueError("Matrix is singular")
             
             A[k], A[k_max] = A[k_max], A[k]
             pi[k], pi[k_max] = pi[k_max], pi[k]
             
+            
             for i in range(k + 1, n):
                 A[i][k] = A[i][k] / A[k][k]
-                for j in range(k + 1, n):
+                # Schur complement
+                for j in range(k + 1, n): 
                     A[i][j] = A[i][j] - A[i][k] * A[k][j]
         
+        # Formation of L, U and P
         L = [[1 if i == j else 0 if i < j else A[i][j] 
               for j in range(n)] for i in range(n)]
         
@@ -61,14 +65,13 @@ class LinearSystemSolver:
         Pb = lup.P * b
         y = Vector([0.0] * b.size)
         
-        # Forward substitution
         for i in range(b.size):
             sum_val = 0
             for j in range(i):
                 sum_val += lup.L.data[i][j] * y.data[j]
             y.data[i] = Pb.data[i] - sum_val
         
-        # Backward substitution
+        # Ux=y
         x = Vector([0.0] * b.size)
         
         for i in range(b.size - 1, -1, -1):
